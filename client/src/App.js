3cde066616143ac127fad5ffdfbb6e72c1c1c1fb
jxhnkndl <jkroyston@gmail.com>
2021-04-22 18:01:39 -0400

    Locate and correct an incorrect file path preventing the books controller from communicating with the API routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
 // Import dependencies
 import React from 'react';
 import API from './utils/API';
+import Button from 'react-bootstrap/Button';
 
 // Create and export App component
 export default function App() {
-  const fetchGigs = async () => {
+  const fetchBooks = async () => {
     try {
-      const gigs = await API.getGigs();
-      console.log(gigs);
-    } catch(err) {
+      const books = await API.getBooks();
+      console.log(books);
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <div className="p-5 text-center">
       <h1>I'M ONLINE!</h1>
-      <Button variant="info" onClick={fetchGigs}>GET GIGS</Button>{' '}
+      <Button variant="info" onClick={fetchBooks}>
+        GET GIGS
+      </Button>
     </div>
   );
 }
--- routes/api/books.js
+++ routes/api/books.js
@@ -1,6 +1,6 @@
 // Import Express router and books controller
 const router = require("express").Router();
-const booksController = require("../../controllers");
+const booksController = require("../../controllers/booksController");
 
 // Routes that match "/api/books" endpoint
 router.route("/")
