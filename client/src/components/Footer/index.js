89383f186e0c3a2c485abb04ef2aec33b7810dc3
jxhnkndl <jkroyston@gmail.com>
2021-04-23 17:04:58 -0400

    Create static footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+// Import dependencies
+import React from 'react';
+
+// Create and export Footer component
+export default function Footer() {
+  return (
+    <footer className="bg-light py-2">
+      <p className="text-white text-center mb-0">Copyright 2021</p>
+    </footer>
+  );
+}
