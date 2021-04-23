dc31c05ef5567299cad6fb5412b33b0f5fce6260
jxhnkndl <jkroyston@gmail.com>
2021-04-22 22:38:43 -0400

    Create new route with id parameter for handling delete requests

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,11 +1,15 @@
 // Import Express router and books controller
 const router = require("express").Router();
+const { route } = require(".");
 const booksController = require("../../controllers/booksController");
 
 // Routes that match "/api/books" endpoint
 router.route("/")
   .get(booksController.getBooks)
-  .post(booksController.addBook)
+  .post(booksController.addBook);
+
+// Routes that match "/api/books/:id" endpoint
+router.route("/:id")
   .delete(booksController.deleteBook);
 
 // Export API router
