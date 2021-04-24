4ddd8574d467169809ffbd8a4a48e6b67cb9ef5d
jxhnkndl <jkroyston@gmail.com>
2021-04-24 17:47:52 -0400

    Create basic structure for Saved page component

--- client/src/components/Saved/index.js
+++ client/src/components/Saved/index.js
@@ -1,13 +1,32 @@
 // Import dependencies
 import React from 'react';
-import BooksContainer from '../BooksContainer';
+import Book from '../Book';
+
+// Temporary book data for designing and testing components
+import bookData from '../../testData';
 
 // Create and export Search page component
-export default function Saved() {
+export default function Saved(props) {
+  // Temporary assign testing data to results state
+  const results = bookData;
+
   return (
-    <section className="bg-light rounded p-3">
-      <h3 className="text-secondary">Saved Books</h3>
-      <BooksContainer />
+    <section>
+      <div className="rounded p-5">
+        <h3 className="text-secondary">Saved Books</h3>
+        {results.map((book, index) => {
+          return (
+            <Book 
+              key={index}
+              title={book.title}
+              authors={book.authors}
+              image={book.image}
+              link={book.link}
+              description={book.description}
+            />
+          );
+        })}
+      </div>
     </section>
   );
 }
