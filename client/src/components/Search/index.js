6846957989b2964b47214dcf0a7c1a1a7f75d8c7
jxhnkndl <jkroyston@gmail.com>
2021-04-24 15:54:58 -0400

    Import local array of books to use in designing, mapping over, and rendering reusable book components"

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,18 +1,37 @@
 // Import dependencies
 import React from 'react';
 import SearchForm from '../SearchForm';
-import BooksContainer from '../BooksContainer';
+import Book from '../Book';
+
+// Temporary book data for designing and testing components
+import bookData from '../../testData';
 
 // Create and export Search page component
 export default function Search() {
+
+  // Temporary assign testing data to results state
+  const results = bookData;
+
   return (
     <section>
       <div className="bg-light rounded p-5 mb-4">
         <h3 className="text-secondary">Book Search</h3>
         <SearchForm />
       </div>
-      <div className="bg-light rounded p-5">
-        <BooksContainer />
+      <div className="books-container bg-light rounded p-5">
+        <h4 className="text-secondary">Results</h4>
+        {results.map((book, index) => {
+          return (
+            <Book 
+              key={index}
+              title={book.title}
+              authors={book.authors}
+              image={book.image}
+              link={book.link}
+              description={book.description}
+            />
+          );
+        })}
       </div>
     </section>
   );
