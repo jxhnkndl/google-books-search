32e4571a3f7a188d8175c7d7a445175727d28bee
jxhnkndl <jkroyston@gmail.com>
2021-04-25 13:32:53 -0400

    Remove dynamically rendered sections of component to simplify deployment testing

--- client/src/components/Saved/index.js
+++ client/src/components/Saved/index.js
@@ -4,14 +4,11 @@ import Book from '../Book';
 
 // Create and export Search page component
 export default function Saved(props) {
-  // Temporary assign testing data to results state
-  const results = bookData;
-
   return (
     <section>
       <div className="rounded p-5">
         <h3 className="text-secondary">Saved Books</h3>
-        {results.map((book, index) => {
+        {/* {results.map((book, index) => {
           return (
             <Book 
               key={index}
@@ -22,7 +19,7 @@ export default function Saved(props) {
               description={book.description}
             />
           );
-        })}
+        })} */}
       </div>
     </section>
   );
